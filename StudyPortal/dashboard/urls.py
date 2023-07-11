e54b1e03cab6828f5eecdf202661ee52b0064ebd
ravan2763 <ravan2763@gmail.com>
2023-07-11 18:08:54 +0530

    ../StudentStudyPortalVenv/Lib/site-packages/crispy_bootstrap4-2022.1.dist-info/ 	../StudentStudyPortalVenv/Lib/site-packages/crispy_bootstrap4/ 	../StudentStudyPortalVenv/Lib/site-packages/crispy_forms/ 	../StudentStudyPortalVenv/Lib/site-packages/django_crispy_forms-2.0.dist-info/ 	dashboard/__pycache__/forms.cpython-311.pyc 	dashboard/forms.py 	dashboard/migrations/0001_initial.py 	dashboard/migrations/0002_alter_notes_options_homework.py 	dashboard/migrations/__pycache__/0001_initial.cpython-311.pyc 	dashboard/migrations/__pycache__/0002_alter_notes_options_homework.cpython-311.pyc

--- StudyPortal/dashboard/urls.py
+++ StudyPortal/dashboard/urls.py
@@ -4,5 +4,12 @@ from . import views
 
 
 urlpatterns =[
-    path('', views.home)
+    path('', views.home, name = 'home'),
+    path('notes', views.notes, name = 'notes'),
+    path('notes_delete/<int:pk>/', views.notes_delete, name='notes_delete'),
+    path('note_detail/<int:pk>/', views.NotesDetailView.as_view(), name='note_detail'),
+    path('homework', views.homework, name= 'homework')
+    
+    
+    
 ]
--- StudyPortal/dashboard/views.py
+++ StudyPortal/dashboard/views.py
@@ -1,6 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Notes, Homework
+from .forms import NotesForm
+from django.contrib import messages
+from django.views import generic
 
 # Create your views here.
 
 def home(request):
     return render(request, 'dashboard/home.html')
+
+def notes(request):
+    if request.method == 'POST':
+        form = NotesForm(request.POST)
+        if form.is_valid():
+            notes = Notes(user = request.user, title = request.POST['title'], description = request.POST['description'])
+            notes.save()
+        messages.success(request, f"Notes Added Successfully By {request.user.username}")
+        return redirect('notes')
+        
+    else:
+        form = NotesForm()
+    notes = Notes.objects.filter(user = request.user)
+    context = {'notes' : notes, 'form':form}
+    return render(request, 'dashboard/notes.html', context)
+
+
+def notes_delete(request, pk = None):
+    Notes.objects.get(id = pk).delete()
+    return redirect('notes')
+
+class NotesDetailView(generic.DetailView):
+    model = Notes
+    
+    
+def homework(request):
+    homeworks =  Homework.objects.filter()
+    if len(homeworks) == 0:
+        homework_done = True
+    else:
+        homework_done = False
+    context = {'homework' : homeworks, 'homework_done' : homework_done}
+    return render(request, 'dashboard/homework.html', context)
+    
